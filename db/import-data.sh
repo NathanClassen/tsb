d85b17931507205816089044af879a4388abd3af
Nathan Classen <hayesclassen@gmail.com>
2024-07-16 22:26:38 -0500

    doing all db initialization in container, no psql dependency; gitignore tsb binary; rm Dockerfile

--- db/import-data.sh
+++ db/import-data.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
 export PGPASSWORD=password
 
-psql -U postgres -h localhost < db/cpu_usage.sql
-
-psql -U postgres -d homework -h localhost -c "\COPY cpu_usage FROM db/cpu_usage.csv CSV HEADER"
+psql -U postgres -d homework -c "\COPY cpu_usage FROM /docker-entrypoint-initdb.d/cpu_usage.csv CSV HEADER"
